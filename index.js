5877e93b30d8ae11f0eda316d70fed4d62916d8a
pmtlab <pmtlab@handshake.dreamhost.com>
2018-06-26 08:34:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var morgan = require("morgan");
+var passport = require("passport");
+var BearerStrategy = require('passport-azure-ad').BearerStrategy;
+
+var options = {
+    identityMetadata: "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration/",
+    clientID: "85327f73-fd44-46b9-a159-28544ff72288",
+    validateIssuer: false,
+    loggingLevel: 'warn',
+    passReqToCallback: false
+};
+
+// Check for client id placeholder
+if (options.clientID === 'YOUR_CLIENT_ID') {
+    console.error("Please update 'options' with the client id (application id) of your application");
+    return;
+}
+
+var bearerStrategy = new BearerStrategy(options,
+    function (token, done) {
+        // Send user info using the second argument
+        done(null, {}, token);
+    }
+);
+
+var app = express();
+app.use(morgan('dev'));
+
+app.use(passport.initialize());
+passport.use(bearerStrategy);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/hello",
+    passport.authenticate('oauth-bearer', {session: false}),
+    function (req, res) {
+        var claims = req.authInfo;
+        console.log('User info: ', req.user);
+        console.log('Validated claims: ', claims);
+        
+        res.status(200).json({'name': claims['name']});
+    }
+);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on port " + port);
+});
---
+++ msalconfig.js
@@ -0,0 +1,4 @@
+var msalconfig = {
+    clientID: "126ba145-989e-452d-9db1-bb9560e26ad4",
+    redirectUri: location.origin
+};
